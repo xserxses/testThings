926aa3c2fa4562f41c0e7118d0098606516340c1
Artur Skowronski <a.skowronski@smartrecruiters.com>
2017-11-23 13:56:40 +0100

    Final functions outlook

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -2,31 +2,57 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const rp = require('request-promise');
+const ApiAiApp = require('actions-on-google').ApiAiApp;
 
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.chuckNorris = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    admin.database().ref('/messages').push({original: original}).then(snapshot => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        res.redirect(303, snapshot.ref);
-    });
-});
-
 exports.chuckNorris = functions.https.onRequest((request, response) => {
-    rp('https://api.chucknorris.io/jokes/random')
+    const app = new ApiAiApp({request: request, response: response});
+    let category;
+    console.log("raw input" + app.getRawInput());
+
+
+    if (!app.getArgument("Category")) {
+        if (app.getContext("chucknorris-followup")) {
+            console.log("parent context exists");
+            category = app.getContextArgument("chucknorris-followup", "Category").value
+        } else {
+            app.ask(app.buildRichResponse()
+                .addSimpleResponse("Please Select Category")
+                .addBasicCard(app.buildBasicCard("Please Select Category")
+                    .setImage("http://www.eastcottvets.co.uk/uploads/Animals/gingerkitten.jpg", "Kitten"))
+                    .addSuggestions([
+                        "explicit",
+                        "dev",
+                        "movie",
+                        "food",
+                        "celebrity",
+                        "science",
+                        "political",
+                        "sport",
+                        "religion",
+                        "animal",
+                        "music",
+                        "history",
+                        "travel",
+                        "career",
+                        "money",
+                        "fashion"
+                    ])
+            )
+        }
+    } else {
+        category = app.getArgument("Category")
+    }
+    console.log(`I found category: ${category}`);
+    rp(`https://api.chucknorris.io/jokes/random?category=${category}`)
         .then(function ($) {
-            response.send(JSON.parse($).value);
+            app.ask(app.buildRichResponse()
+                .addSimpleResponse(JSON.parse($).value)
+                .addBasicCard(app.buildBasicCard(JSON.parse($).value)
+                    .setImage("https://assets.chucknorris.host/img/chucknorris_logo_coloured_small.png", "Kitten"))
+                .addSuggestions(['Next', "Exit"])
+            )
         });
-});
+})
+;
